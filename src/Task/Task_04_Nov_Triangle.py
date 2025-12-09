2640a07c159a278777fc1a4f3f7fd8eb9360b98c
Deepsaa <deepsa.sahoo90@gmail.com>
2025-12-09 23:06:01 +0530

    04.11.2025,these are the new files.

---
+++ src/Task/Task_04_Nov_Triangle.py
@@ -0,0 +1,16 @@
+#printing the right trangle
+#*
+#**
+#***
+#****
+#*****
+
+rows= int(input("Enter the rows for the right angle triangle"))
+
+for i in range(1,rows+1):
+    for j in range(i):
+      print("*", end="")
+    print()
+
+#for i in range(1,6):
+#    print("*" * i)
