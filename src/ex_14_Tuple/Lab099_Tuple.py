70befca34f66fedf81bd61477f310651fd8173c9
Deepsaa <deepsa.sahoo90@gmail.com>
2025-12-12 15:03:08 +0530

    06.11.2025,these are the new files.

---
+++ src/ex_14_Tuple/Lab099_Tuple.py
@@ -0,0 +1,11 @@
+my_tuple = (1,2,3,4,5)
+print (my_tuple)
+#my_tuple[0] = 12  Type Error tuple object does not support
+
+#With mix Data type
+info = ("Deepsa",34,True,5.4)
+print(info)
+
+#Tuple with one element
+single = (3,)
+print(type(single))#Class tuple
